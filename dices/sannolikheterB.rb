f3e4ac42bb7db330582e4bc2181bca883a32cf77
itgsod-filip-forsberg <filip.forsberg@elev.it-gymnasiet.se>
2015-03-24 13:47:11 +0100

    Klar med uppgift 1 b

---
+++ dices/sannolikheterB.rb
@@ -0,0 +1,7 @@
+puts "Vill du snura en tärning och kasta den i luften två gånger i rad och få att sidan med sex pricakr på den komma upp båda gångerna? JAAAAAA DET VILL DU!"
+
+a = 6
+b = 1
+
+
+puts "Det är #{(((b/a.to_f)**2)*100).round}% chans att du lyckas"
